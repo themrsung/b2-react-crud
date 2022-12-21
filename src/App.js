a9447e1b04cb4de4d5c783808c909d186e447011
eidoj <dudwns5013@gmail.com>
2022-12-21 20:28:25 +0900

    jyj

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />}></Route>
-
           <Route path="login" element={<Login />}></Route>
           <Route path="register" element={<Register />}></Route>
         </Routes>
