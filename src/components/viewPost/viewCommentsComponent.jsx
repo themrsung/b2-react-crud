8886d7311a8cf02dec1837fe4f27fec9e2d58059
kimwonjuun <117059038+kimwonjuun@users.noreply.github.com>
2022-12-22 19:07:05 +0900

    댓글창 만들기 2

--- src/components/viewPost/viewCommentsComponent.jsx
+++ src/components/viewPost/viewCommentsComponent.jsx
@@ -1,6 +1,10 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { SERVER_URL } from '../../serverUrl'
+import { v4 as uuidv4 } from 'uuid'
+import { getCurrentUserState } from '../../redux/config/configStore'
+import { useNavigate } from 'react-router-dom'
+import Comment from './comment'
 
 const ViewCommentsComponent = ({ postId }) => {
   const [post, setPost] = useState({})
@@ -19,8 +23,28 @@ const ViewCommentsComponent = ({ postId }) => {
 
   const [userComment, setUserComment] = useState('')
 
-  const onWriteComment = () => {
-    console.log('test')
+  let navigate = useNavigate()
+
+  const onWriteComment = async () => {
+    const user = getCurrentUserState()
+    if (user.id === '') {
+      navigate('/login')
+    }
+
+    const comment = {
+      content: userComment,
+      id: uuidv4(),
+      author: getCurrentUserState().id,
+      createdAt: Date.now()
+    }
+
+    let newComments = comments
+    newComments.push(comment)
+    console.log(newComments)
+
+    const res = await axios.patch(SERVER_URL + '/posts/' + postId, {
+      comments: newComments
+    })
   }
 
   return (
@@ -46,7 +70,7 @@ const ViewCommentsComponent = ({ postId }) => {
       </div>
       <div>
         {comments.map((comment) => {
-          return <div key={comment.id}></div>
+          return <Comment key={comment.id} comment={comment} />
         })}
       </div>
     </div>
