893b59936e65a0fe5703d647e30c83f9fb9e41cf
MJ Sung <themrsung@gmail.com>
2022-12-22 16:17:15 +0900

    not found

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,8 +1,11 @@
+import logo from '../not-found.svg'
+import './style/NotFound.css'
+
 const NotFound = function () {
   return (
     <div className="NotFound">
       <div className="NotFoundLeft">
-        <img className="NotFoundLogo" alt="Page not found"></img>
+        <img className="NotFoundLogo" src={logo} alt="Page not found"></img>
         <h3>페이지를 찾을 수 없습니다. 더 열심히 찾아보세요ㅋㅋ</h3>
       </div>
     </div>
