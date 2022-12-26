13e31ea812e9fc262f176e04e0afd73131c8c8fe
MJ Sung <themrsung@gmail.com>
2022-12-26 20:07:06 +0900

    gitignore

--- src/serverUrl.js
+++ src/serverUrl.js
@@ -1,2 +1,2 @@
-// export const SERVER_URL = 'http://localhost:3001'
-export const SERVER_URL = 'https://b2-react-backend.herokuapp.com'
+export const SERVER_URL = 'http://localhost:3001'
+// export const SERVER_URL = 'https://b2-react-backend.herokuapp.com'
